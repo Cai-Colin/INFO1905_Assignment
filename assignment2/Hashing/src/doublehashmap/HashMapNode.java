829480cec48aa699fc1f5a8ee305ad3e2d60fc0c
Cai-Colin <ccai1997@hotmail.com>
2015-10-10 13:36:35 +1100

    Commented Code
    All code is now commented

--- assignment2/Hashing/src/doublehashmap/HashMapNode.java
+++ assignment2/Hashing/src/doublehashmap/HashMapNode.java
@@ -5,21 +5,26 @@ public class HashMapNode<K extends Comparable<K>, V>
 	private K key;
 	private V value;
 	
+	//Constructor
 	public HashMapNode(K key, V value)
 	{
 		this.key = key;
 		this.value = value;
 	}
 	
+	//Get method for the key
 	public K getKey()
 	{
 		return this.key;
 	}
+	
+	//Get method for the value
 	public V getValue()
 	{
 		return this.value;
 	}
 
+	//Set value for the method
 	public void setValue(V newValue)
 	{
 		this.value = newValue;
